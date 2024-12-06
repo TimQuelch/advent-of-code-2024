f5a47f62a54e159e301038675705ad64dfc1760d
Tim Quelch <timothy.quelch@versent.com.au>
2024-12-06 14:57:56 +1100

    Optimise d01

--- src/days/d01.rs
+++ src/days/d01.rs
@@ -3,21 +3,37 @@ use std::{
     iter::zip,
 };
 
+struct PopIterator<T> {
+    heap: BinaryHeap<T>,
+}
+
+impl<T> PopIterator<T> {
+    fn new(heap: BinaryHeap<T>) -> Self {
+        PopIterator { heap }
+    }
+}
+
+impl<T> Iterator for PopIterator<T>
+where
+    T: Ord,
+{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.heap.pop()
+    }
+}
+
 pub fn part1(input: &str) -> i32 {
-    let mut lists: [BinaryHeap<i32>; 2] = [(); 2].map(|_| BinaryHeap::new());
+    let mut list1 = BinaryHeap::new();
+    let mut list2 = BinaryHeap::new();
 
     input.lines().for_each(|l| {
-        l.split_whitespace()
-            .map(|s| s.parse())
-            .enumerate()
-            .for_each(|(i, val)| lists[i].push(val.unwrap()))
+        let mut iter = l.split_whitespace().map(|s| s.parse::<i32>().unwrap());
+        list1.push(iter.next().unwrap());
+        list2.push(iter.next().unwrap());
     });
 
-    assert!(lists[0].len() == lists[1].len());
-
-    let sorted = lists.map(|l| l.into_sorted_vec());
-
-    return zip(sorted[0].iter(), sorted[1].iter())
+    return zip(PopIterator::new(list1), PopIterator::new(list2))
         .map(|(a, b)| (a - b).abs())
         .sum();
 }
@@ -26,22 +42,18 @@ pub fn part2(input: &str) -> i32 {
     let mut map: HashMap<i32, (i32, i32)> = HashMap::new();
 
     input.lines().for_each(|l| {
-        let vals: Vec<i32> = l.split_whitespace().map(|s| s.parse().unwrap()).collect();
+        let mut iter = l.split_whitespace().map(|s| s.parse().unwrap());
 
-        assert!(vals.len() == 2);
-
-        map.entry(vals[0])
+        map.entry(iter.next().unwrap())
             .and_modify(|(l, _)| *l += 1)
             .or_insert((1, 0));
-        map.entry(vals[1])
+
+        map.entry(iter.next().unwrap())
             .and_modify(|(_, r)| *r += 1)
             .or_insert((0, 1));
     });
 
-    return map
-        .drain()
-        .map(|(x, (y, z))| x * y * z)
-        .fold(0, |acc, x| acc + x);
+    return map.drain().map(|(x, (y, z))| x * y * z).sum();
 }
 
 #[cfg(test)]
