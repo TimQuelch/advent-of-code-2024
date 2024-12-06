60d709658bf2661ee70c6bf661826a8279a66283
Tim Quelch <timothy.quelch@versent.com.au>
2024-12-06 14:47:17 +1100

    Optimise d02

--- src/days/d02.rs
+++ src/days/d02.rs
@@ -1,55 +1,56 @@
 use itertools::Itertools;
-use std::iter::{Chain, Iterator, Skip, Take};
+use std::{
+    iter::{Chain, Iterator},
+    slice::Iter,
+};
 
 fn is_safe<I>(iter: I) -> bool
 where
-    I: Iterator<Item = i32> + Clone,
+    I: Iterator<Item = i8> + Clone,
 {
-    let diff: Vec<_> = iter.tuple_windows().map(|(a, b)| a - b).collect();
+    let mut diffiter = iter.tuple_windows().map(|(a, b)| a - b);
 
-    (diff.iter().all(|x| *x > 0) || diff.iter().all(|x| *x < 0))
-        && diff.iter().all(|x| x.abs() <= 3)
+    match diffiter.next().unwrap() {
+        x if (x.abs() >= 3) => return false,
+        x if (x > 0) => diffiter.all(|x| x > 0 && x <= 3),
+        x if (x < 0) => diffiter.all(|x| x < 0 && x >= -3),
+        _ => return false,
+    }
 }
 
 pub fn part1(input: &str) -> i32 {
     return input
         .lines()
-        .map(|l| l.split_whitespace().map(|n| n.parse::<i32>().unwrap()))
-        .filter(|r| is_safe(r.clone()))
+        .map(|l| l.split_whitespace().map(|n| n.parse::<i8>().unwrap()))
+        .filter_map(|r| match is_safe(r.clone()) {
+            true => Some(()),
+            false => None,
+        })
         .count()
         .try_into()
         .unwrap();
 }
 
-struct TryRemovingEach<T>
-where
-    T: Iterator + Clone,
-{
-    iter: T,
+struct TryRemovingEach<'a, T> {
+    slice: &'a Vec<T>,
     current: usize,
 }
 
-impl<T> TryRemovingEach<T>
-where
-    T: Iterator + Clone,
-{
-    fn new(iter: T) -> Self {
-        TryRemovingEach { iter, current: 0 }
+impl<'a, T> TryRemovingEach<'a, T> {
+    fn new(slice: &'a Vec<T>) -> Self {
+        TryRemovingEach { slice, current: 0 }
     }
 }
 
-impl<T> Iterator for TryRemovingEach<T>
-where
-    T: Iterator + Clone,
-{
-    type Item = Chain<Take<T>, Skip<T>>;
+impl<'a, T> Iterator for TryRemovingEach<'a, T> {
+    type Item = Chain<Iter<'a, T>, Iter<'a, T>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.iter.clone().nth(self.current).is_none() {
+        if self.current >= self.slice.len() {
             return None;
         }
-        let before = self.iter.clone().take(self.current);
-        let after = self.iter.clone().skip(self.current + 1);
+        let before = self.slice[..self.current].iter();
+        let after = self.slice[self.current + 1..].iter();
         let combined = before.chain(after);
         self.current += 1;
         return Some(combined);
@@ -57,10 +58,18 @@ where
 }
 
 pub fn part2(input: &str) -> i32 {
+    let mut working_space: Vec<i8> = vec![];
     return input
         .lines()
-        .map(|l| l.split_whitespace().map(|n| n.parse::<i32>().unwrap()))
-        .filter(|r| TryRemovingEach::new(r.clone()).any(|newr| is_safe(newr)))
+        .map(|l| l.split_whitespace().map(|n| n.parse::<i8>().unwrap()))
+        .filter_map(|r| {
+            working_space.clear();
+            working_space.extend(r);
+            match TryRemovingEach::new(&working_space).any(|newr| is_safe(newr.cloned())) {
+                true => Some(()),
+                false => None,
+            }
+        })
         .count()
         .try_into()
         .unwrap();
